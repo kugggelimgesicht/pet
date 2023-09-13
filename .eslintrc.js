e099e5e1535033a0fe0c6e539d4dd04738feb440
daria.fedzyarina <daria.fedzyarina@aventus.work>
2023-09-14 00:47:20 +0300

    add testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,10 +40,18 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid'] }],
         'max-len': ['error', { code: 150, ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
